e01a5f7e719b331a05aa8154ab88098625c15a45
sunyuchang123 <vwm9888@autuni.ac.nz>
2023-10-11 01:04:01 +1300

    stage 1
    I added productData abstract class ,and panelbackground.

---
+++ team 22/src/View/BackgroundPanel.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package View;
+
+import javax.swing.JPanel;
+import javax.swing.ImageIcon;
+import java.awt.Graphics;
+import java.awt.Image;
+
+/**
+ *
+ * @author yucha
+ */
+public class BackgroundPanel extends JPanel {
+      private Image backgroundImage;
+
+    public BackgroundPanel(ImageIcon gifIcon) {
+        this.backgroundImage = gifIcon.getImage();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
+    }
+}
