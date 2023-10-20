6540e4c959c518cb2e479fdd8ef94bf523e8d754
sunyuchang123 <vwm9888@autuni.ac.nz>
2023-10-20 16:40:44 +1300

    new class for inventory
    ClientSelection class

---
+++ team 22/src/Inventory/ClientSelection.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Inventory;
+
+/**
+ *
+ * @author yucha
+ */
+public class ClientSelection {
+    
+    public final String Selection = "Inventory";
+
+    public String getProductInfo(ProductData item) {
+        StringBuilder returnString = new StringBuilder();
+        returnString.append("Product name: ").append(item.getName());
+        returnString.append("\nProduct quantity: ").append(item.getQuantity());
+        returnString.append("\nProduct price: $").append(item.getPrice());
+        returnString.append("\nProduct Code: ").append(item.getCode());
+
+        return returnString.toString();
+    }
+
+    /**
+     * Retrieves the current selection type.
+     * 
+     * @return The current selection type.
+     */
+    public String getCurrentSelection() {
+        return Selection;
+    }
+}
+
+class ProductData {
+    public String getName() {
+        return "";
+    }
+
+    public int getQuantity() {
+        return 0;
+    }
+
+    public double getPrice() {
+        return 0.0;
+    }
+
+    public String getCode() {
+        return "";
+    }
+}
